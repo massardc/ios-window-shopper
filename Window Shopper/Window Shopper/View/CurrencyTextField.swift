bb4cbac56d328da4743476dba21acf6d65b72b66
Clement M <massardc@users.noreply.github.com>
2017-08-14 15:13:00 +0200

    Initialized main screen and created Custom text field class

---
+++ Window Shopper/Window Shopper/View/CurrencyTextField.swift
@@ -0,0 +1,29 @@
+//
+//  CurrencyTextField.swift
+//  Window Shopper
+//
+//  Created by School on 14/08/2017.
+//  Copyright © 2017 ClementM. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if placeholder == nil {
+            placeholder = " "
+        }
+        
+        if let p = placeholder { let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+
+}
