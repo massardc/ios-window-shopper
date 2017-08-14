b52c20ac4a819c5010909ded8221921d1145d4bc
Clement M <massardc@users.noreply.github.com>
2017-08-14 19:12:17 +0200

    Added currency symbol in labels programmatically

--- Window Shopper/Window Shopper/View/CurrencyTextField.swift
+++ Window Shopper/Window Shopper/View/CurrencyTextField.swift
@@ -10,6 +10,22 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.7318410212, green: 0.7318410212, blue: 0.7318410212, alpha: 0.7475792254)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -27,6 +43,8 @@ class CurrencyTextField: UITextField {
         if placeholder == nil {
             placeholder = " "
         }
+        
+        clipsToBounds = true
         
         if let p = placeholder { let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
             attributedPlaceholder = place
